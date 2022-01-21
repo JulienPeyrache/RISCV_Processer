668adf2c18a424fb23042dfda36c35d99adabf74
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-01-21 11:20:44 +0100

    ajout autotest add

---
+++ implem/program/autotest/add.s
@@ -0,0 +1,14 @@
+# TAG = add
+	.text
+
+	addi x1, x0, 5       #Test chargement d'une valeur nulle
+	add x31, x0, x0
+	add x31, x0, x1 #Test chargement d'une valeur maximal sur 20 bits
+	add x31, x31, x31 #Test chargement d'une valeur quelconque
+
+	# max_cycle 50
+	# pout_start
+	# 00000000
+	# 00000005
+	# 0000000A
+	# pout_end
