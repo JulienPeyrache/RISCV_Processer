1f63c0b454f6547eb5a65984952a5f4f809c0172
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-01-25 19:53:45 +0100

    decode

--- implem/vhd/CPU_PC.vhd
+++ implem/vhd/CPU_PC.vhd
@@ -248,7 +248,7 @@ begin
 				        end if;
 			        end if;
 				
-                else then
+                else
                     state_d <= S_Error; -- Pour detecter les rates du decodage
                 end if;
 
