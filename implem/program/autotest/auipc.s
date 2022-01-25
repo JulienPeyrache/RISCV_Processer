085f3018dd46551276152cc02eef9b89720a0d66
Julien Peyrache <julien.peyrache@student-cs.fr>
2022-01-25 23:28:48 +0100

    yéyé

--- implem/program/autotest/auipc.s
+++ implem/program/autotest/auipc.s
@@ -1,9 +1,17 @@
 #TAG = auipc
     .text
 
-    auipc x31, 1
+    lui x31, 0x00003
+    auipc x31, 0x00006
+    auipc x31, 0x00006
+    auipc x31, 0x00006
+    auipc x31, 0x00006
 
     # max_cycle 50
-	# pout_start
-	# 00000000
-	# pout_end
+    # pout_start
+    # 00003000
+    # 00007008
+    # 0000700c
+    # 00007010
+    # 00007014
+    # pout_end
